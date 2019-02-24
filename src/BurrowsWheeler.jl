a9062338b8180fab37229c8d44f429eb0f966311
Mateusz Kaduk <mateusz.kaduk@ki.se>
2019-02-24 09:51:27 +0100

    BurrowsWheeler: Add lexicologically sorted suffix array function returning indices

--- src/BurrowsWheeler.jl
+++ src/BurrowsWheeler.jl
@@ -1,21 +1,30 @@
 module BurrowsWheeler
 using BioSequences
-export bwt
-function bwt(word::BioSequence{DNAAlphabet{4}})
-    if DNA_Gap in word
-        throw(ArgumentError("input must not contain gaps"))
+export bwt, sa
+    function sa(word::BioSequence{DNAAlphabet{4}})
+        w = copy(word)
+        if DNA_Gap in w
+            throw(ArgumentError("input must not contain gaps"))
+        end
+        push!(w, DNA_Gap)
+        S = sort!([(i, w[i:end]) for i in 1:(length(w))], by = x -> x[2])
+        return(first.(S))
     end
-    w = convert(Vector{DNA}, word)
-    push!(w, DNA_Gap)
-    l = Vector{DNA}()
-    i = length(w)
-    while i > 0
-        c = pop!(w)
-        pushfirst!(w, c)
-        i = i - 1
-        append!(l, w)
+    function bwt(word::BioSequence{DNAAlphabet{4}})
+        if DNA_Gap in word
+            throw(ArgumentError("input must not contain gaps"))
+        end
+        w = convert(Vector{DNA}, word)
+        push!(w, DNA_Gap)
+        l = Vector{DNA}()
+        i = length(w)
+        while i > 0
+            c = pop!(w)
+            pushfirst!(w, c)
+            i = i - 1
+            append!(l, w)
+        end
+        M = reshape(l, length(w), length(w))
+        return(sortslices(M, dims=2)[end, :])
     end
-    M = reshape(l, length(w), length(w))
-    return(sortslices(M, dims=2)[end, :])
-end
 end
--- test/testBurrowsWheeler.jl
+++ test/testBurrowsWheeler.jl
@@ -3,3 +3,5 @@ using Test
 BWT = [DNA_A, DNA_T, DNA_T, DNA_A, DNA_Gap, DNA_A, DNA_A]
 @test bwt(dna"ATAATA") == BWT
 @test_throws ArgumentError bwt(dna"ATC-A")
+@test sa(dna"ATAATA") == [7,6,3,4,1,5,2]
+@test_throws ArgumentError sa(dna"ATC-A")
